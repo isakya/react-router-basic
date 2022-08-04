1b72ce78c84f5ad5fd292c0a5deae191b756180d
konglong <1016361268@qq.com>
2022-08-05 07:50:25 +0800

    url问号传参方式

--- src/About.js
+++ src/About.js
@@ -1,6 +1,13 @@
+import { useSearchParams } from 'react-router-dom'
 function About() {
+  const [params] = useSearchParams()
+  // params 是一个对象 对象里面有一个get方法
+  // 用来获取对应的参数
+  // 把参数的名称作为get方法的实参传过来
+  const id = params.get('id')
+  const name = params.get('name')
   return (
-    <div>about</div>
+    <div>about得到的参id值为{id},name值为{name}</div>
   )
 }
 
--- src/Login.js
+++ src/Login.js
@@ -6,7 +6,7 @@ function Login() {
   // 跳转到关于页面
   function goAbout() {
     // 3. 调用跳转函数，传入目标路径
-    navigate('/about', { replace: true })
+    navigate('/about?id=1001&name=zs', { replace: true })
   }
   return (
     <div>
